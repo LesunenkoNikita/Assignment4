e049b62a3231cccceab1b7dd1cc9cee8cc12e763
LesunenkoNikita <nikitoslesunenko@gmail.com>
2024-07-05 14:27:25 +0300

    created constructor and destructor for caesarcipher class

--- Assignment4/CaesarCipher.cpp
+++ Assignment4/CaesarCipher.cpp
@@ -1,1 +1,27 @@
 #include "CaesarCipher.h"
+#include <iostream>
+using namespace std;
+
+CaesarCipher::CaesarCipher() 
+{
+	handle = LoadLibrary(TEXT("C:\\Users\\User\\source\\repos\\Assignment3\\Assignment3\\Assignment3\\caesar.dll"));
+	if (handle == nullptr || handle == INVALID_HANDLE_VALUE)
+	{
+		cout << "Lib not found" << endl;
+	}
+	encrypt_ptr = (encrypt_ptr_t)GetProcAddress(handle, "encrypt");
+	if (encrypt_ptr == nullptr)
+	{
+		cout << "Function not found" << endl;
+	}
+	decrypt_ptr = (decrypt_ptr_t)GetProcAddress(handle, "decrypt");
+	if (decrypt_ptr == nullptr)
+	{
+		cout << "Function not found" << endl;
+	}	
+}
+
+CaesarCipher::~CaesarCipher() 
+{
+	FreeLibrary(handle);
+}
